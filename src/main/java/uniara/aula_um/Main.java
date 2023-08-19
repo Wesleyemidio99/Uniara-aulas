5e6127244a6838a68a7ee637989d75938201f57b
wesley <wesleyemidio99@hotmail.com>
2023-08-19 12:37:39 -0300

    Primeira aula

---
+++ src/main/java/uniara/aula_um/Main.java
@@ -0,0 +1,11 @@
+package uniara.aula_um;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null,"Hello World");
+
+    }
+}
